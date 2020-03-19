ec7f98cda6f1a55afc4048d45cd187bc25d8bf6f
Franck M <franckisys@gmail.com>
2020-03-19 16:52:27 +0100

    add rm firts installation file

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo "######## Decompress"
 tar xzf voipmonitor-*-static.tar.gz
 echo "######## Download voipservices conf"
 cd voipmonitor-*-static/
+rm install-script.sh
 wget -P etc/ https://raw.githubusercontent.com/fouille/vps_monit_install_sniffer/master/voipservices.conf
 wget -P etc/init.d https://raw.githubusercontent.com/fouille/vps_monit_install_sniffer/master/etc/init.d/voipservices
 wget https://raw.githubusercontent.com/fouille/vps_monit_install_sniffer/master/install-script.sh
