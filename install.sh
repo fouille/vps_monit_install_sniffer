655a0179ef41f7242d15e65683fefc0b44bc3163
Franck M <franckisys@gmail.com>
2020-03-19 16:35:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #/bin/sh
 
 echo "######## run to /tmp"
-cd /tmp/ 
+cd /tmp/
 echo "######## Download sniffer"
 wget https://www.voipmonitor.org/current-stable-sniffer-static-64bit.tar.gz --content-disposition --no-check-certificate
 echo "######## Decompress"
 tar xzf voipmonitor-*-static.tar.gz
+echo "######## Download voipservices conf"
+cd voipmonitor-*-static/
+wget -P etc/ https://raw.githubusercontent.com/fouille/vps_monit_install_sniffer/master/voipservices.conf
+wget -P etc/init.d https://raw.githubusercontent.com/fouille/vps_monit_install_sniffer/master/etc/init.d/voipservices
+wget https://raw.githubusercontent.com/fouille/vps_monit_install_sniffer/master/install-script.sh
+rm etc/voipmonitor.conf
+rm etc/init.d/voipmonitor
 echo "######## Run"
-./voipmonitor-*-static/install-script.sh
+./install-script.sh
 echo "######## Finish"
